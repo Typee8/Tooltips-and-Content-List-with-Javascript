bda48e68e10a8f61e48422b51d1388112ffa0d5d
Typee8 <wojciechjasiak1324@gmail.com>
2024-01-23 14:09:47 +0100

    Finish task

---
+++ assets/js/introduction.js
@@ -0,0 +1,36 @@
+const tooltipListTest = document.querySelector(".tooltip");
+const tooltipList = document.querySelectorAll(".tooltip");
+
+tooltipList.forEach(function (element) {
+  showTooltip(element);
+});
+
+function showTooltip(element) {
+  if (element.hasAttribute("data-url")) {
+    const tooltipText = element.innerText;
+    element.innerText = "";
+    const a = document.createElement("a");
+    a.innerText = tooltipText;
+    a.setAttribute("href", element.dataset.url);
+    element.appendChild(a);
+  }
+
+  if (element.hasAttribute("data-tooltip-type")) {
+    const tooltipType = element.dataset.tooltipType;
+    const span = document.createElement("span");
+    span.setAttribute("class", `tooltip__box tooltip__box--${tooltipType}`);
+
+    if (tooltipType === "text") {
+      const spanText = element.dataset.tooltipContent;
+      span.innerText = spanText;
+    } else if (tooltipType === "image") {
+      const img = document.createElement("img");
+      img.setAttribute("class", "tooltip__image");
+      const imgSrc = element.dataset.tooltipContent;
+      img.setAttribute("src", imgSrc);
+      span.appendChild(img);
+    }
+
+    element.appendChild(span);
+  }
+}
--- assets/js/list.js
+++ assets/js/list.js
@@ -41,5 +41,79 @@ const list = [
         parentId: 24,
         text: 'Osadzanie',
         link: '#Osadzanie',
+    },
+    {
+        id: 60,
+        parentId: null,
+        text: 'TestParent',
+        link: '#'
+    },
+    {
+        id: 61,
+        parentId: 60,
+        text: 'Testchild1',
+        link: '#'
+    },
+    {
+        id: 62,
+        parentId: 60,
+        text: 'Testchild2',
+        link: '#'
+    },
+    {
+        id: 63,
+        parentId: 60,
+        text: 'Testchild3',
+        link: '#j'
     }
 ];
+
+const contentListLocation = document.querySelector(".article__list");
+const contentList = document.createElement('ul');
+contentListLocation.appendChild(contentList);
+
+list.forEach (function (element) {
+  if (element.parentId === null) {
+    const li = document.createElement('li');
+    if (element.id) {
+      li.setAttribute('data-id', element.id);
+    }
+
+    contentList.appendChild(li);
+
+    const a = document.createElement('a');
+    if (element.link) {
+      a.setAttribute('href', element.link);
+    }
+    if (element.text) {
+      a.innerText = element.text;
+    }
+
+    li.appendChild(a);
+  }
+})
+
+const contentLiList = contentList.querySelectorAll("[data-id]");
+contentLiList.forEach (function (element) {
+  const id = Number(element.dataset.id);
+  const childrenList = [];
+  list.forEach (function (elem) {
+    if (elem.parentId === id) {
+      return childrenList.push(elem);
+    }
+  })
+
+  if (childrenList.length > 0) {
+    const ul = document.createElement('ul');
+    element.appendChild(ul);
+
+    childrenList.forEach (function (item) {
+      const li = document.createElement('li');
+      ul.appendChild(li);
+      const a = document.createElement('a');
+      a.setAttribute('href', item.parentId);
+      a.innerText = item.text;
+      li.appendChild(a);
+    })
+  }
+})
