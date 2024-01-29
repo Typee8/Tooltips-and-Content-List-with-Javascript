bab71f76d2f9ccd5f304c8cd3f871c97cafeba4b
Typee8 <wojciechjasiak1324@gmail.com>
2024-01-29 12:37:21 +0100

    Function decomposition in list.js

--- assets/js/list.js
+++ assets/js/list.js
@@ -1,119 +1,122 @@
 const list = [
-    {
-        id: 1,
-        parentId: null,
-        text: 'Zastosowanie',
-        link: '#Zastosowanie',
-    },
-    {
-        id: 44,
-        parentId: null,
-        text: 'Historia',
-        link: '#Historia',
-    },
-    {
-        id: 7,
-        parentId: 44,
-        text: 'Dialekty',
-        link: '#Dialekty',
-    },
-    {
-        id: 31,
-        parentId: 44,
-        text: 'Java',
-        link: '#Java',
-    },
-    {
-        id: 24,
-        parentId: null,
-        text: 'JavaScript dla WWW',
-        link: '#JavaScript_dla_WWW',
-
-    },
-    {
-        id: 10,
-        parentId: 24,
-        text: 'Interakcja',
-        link: '#Interakcja'
-    },
-    {
-        id: 25,
-        parentId: 24,
-        text: 'Osadzanie',
-        link: '#Osadzanie',
-    },
-    {
-        id: 60,
-        parentId: null,
-        text: 'TestParent',
-        link: '#'
-    },
-    {
-        id: 61,
-        parentId: 60,
-        text: 'Testchild1',
-        link: '#'
-    },
-    {
-        id: 62,
-        parentId: 60,
-        text: 'Testchild2',
-        link: '#'
-    },
-    {
-        id: 63,
-        parentId: 60,
-        text: 'Testchild3',
-        link: '#j'
-    }
+  {
+    id: 1,
+    parentId: null,
+    text: "Zastosowanie",
+    link: "#Zastosowanie",
+  },
+  {
+    id: 44,
+    parentId: null,
+    text: "Historia",
+    link: "#Historia",
+  },
+  {
+    id: 7,
+    parentId: 44,
+    text: "Dialekty",
+    link: "#Dialekty",
+  },
+  {
+    id: 31,
+    parentId: 44,
+    text: "Java",
+    link: "#Java",
+  },
+  {
+    id: 24,
+    parentId: null,
+    text: "JavaScript dla WWW",
+    link: "#JavaScript_dla_WWW",
+  },
+  {
+    id: 10,
+    parentId: 24,
+    text: "Interakcja",
+    link: "#Interakcja",
+  },
+  {
+    id: 25,
+    parentId: 24,
+    text: "Osadzanie",
+    link: "#Osadzanie",
+  },
+  {
+    id: 60,
+    parentId: null,
+    text: "TestParent",
+    link: "#",
+  },
+  {
+    id: 61,
+    parentId: 60,
+    text: "Testchild1",
+    link: "#",
+  },
+  {
+    id: 62,
+    parentId: 60,
+    text: "Testchild2",
+    link: "#",
+  },
+  {
+    id: 63,
+    parentId: 60,
+    text: "Testchild3",
+    link: "#j",
+  },
 ];
 
-const contentListLocation = document.querySelector(".article__list");
-const contentList = document.createElement('ul');
-contentListLocation.appendChild(contentList);
+const contentListPlace = document.querySelector(".article__list");
+const contentList = document.createElement("ul");
+contentListPlace.appendChild(contentList);
 
-list.forEach (function (element) {
+list.forEach(function (element) {
   if (element.parentId === null) {
-    const li = document.createElement('li');
-    if (element.id) {
-      li.setAttribute('data-id', element.id);
-    }
-
-    contentList.appendChild(li);
-
-    const a = document.createElement('a');
-    if (element.link) {
-      a.setAttribute('href', element.link);
-    }
-    if (element.text) {
-      a.innerText = element.text;
-    }
-
-    li.appendChild(a);
+    createContentList(element, contentList);
   }
-})
+});
 
-const contentLiList = contentList.querySelectorAll("[data-id]");
-contentLiList.forEach (function (element) {
-  const id = Number(element.dataset.id);
-  const childrenList = [];
-  list.forEach (function (elem) {
-    if (elem.parentId === id) {
-      return childrenList.push(elem);
-    }
-  })
+const contentChildrenPlaces = contentList.querySelectorAll("[data-id]");
+
+contentChildrenPlaces.forEach(function (element) {
+  const childrenList = createChildrenList(element);
 
   if (childrenList.length > 0) {
-    const ul = document.createElement('ul');
+    const ul = document.createElement("ul");
     element.appendChild(ul);
 
-    childrenList.forEach (function (item) {
-      const li = document.createElement('li');
-      ul.appendChild(li);
-      const a = document.createElement('a');
-      a.setAttribute('href', item.parentId);
-      a.innerText = item.text;
-      li.appendChild(a);
-    })
+    childrenList.forEach(function (element) {
+      createContentList(element, ul);
+    });
   }
-})
+});
+
+function createContentList(element, parent) {
+  const li = document.createElement("li");
+  if (element.id && element.parentId === null) {
+    li.setAttribute("data-id", element.id);
+  }
+  parent.appendChild(li);
+
+  const a = document.createElement("a");
+  if (element.link) {
+    a.setAttribute("href", element.link);
+  }
+  if (element.text) {
+    a.innerText = element.text;
+  }
+
+  li.appendChild(a);
+}
+
+function createChildrenList (element) {
+    const id = Number(element.dataset.id);
+    const childrenList = [];
+    list.forEach(function (elem) {
+      if (elem.parentId === id) {
+        return childrenList.push(elem);
+      }
+    });
+    return childrenList;
+}
