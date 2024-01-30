7e2b07ab55e734e995b4321f605e0ac10d37016c
Typee8 <wojciechjasiak1324@gmail.com>
2024-01-30 10:35:10 +0100

    Replace conditionals from createTooltipImg and createTooltipText to createTooltip

--- assets/js/introduction.js
+++ assets/js/introduction.js
@@ -25,25 +25,24 @@ function createTooltip(element) {
   const span = document.createElement("span");
   span.setAttribute("class", `tooltip__box tooltip__box--${tooltipType}`);
 
-  createTooltipText(element, tooltipType, span);
-  createTooltipImg(element, tooltipType, span);
-}
-
-function createTooltipText(element, tooltipType, span) {
   if (tooltipType === "text") {
-    const spanText = element.dataset.tooltipContent;
-    span.innerText = spanText;
-    element.appendChild(span);
+    createTooltipText(element, span);
+  } else if (tooltipType === "image") {
+    createTooltipImg(element, span);
   }
 }
 
-function createTooltipImg(element, tooltipType, span) {
-  if (tooltipType === "image") {
-    const img = document.createElement("img");
-    img.setAttribute("class", "tooltip__image");
-    const imgSrc = element.dataset.tooltipContent;
-    img.setAttribute("src", imgSrc);
-    span.appendChild(img);
-    element.appendChild(span);
-  }
+function createTooltipText(element, span) {
+  const spanText = element.dataset.tooltipContent;
+  span.innerText = spanText;
+  element.appendChild(span);
+}
+
+function createTooltipImg(element, span) {
+  const img = document.createElement("img");
+  img.setAttribute("class", "tooltip__image");
+  const imgSrc = element.dataset.tooltipContent;
+  img.setAttribute("src", imgSrc);
+  span.appendChild(img);
+  element.appendChild(span);
 }
